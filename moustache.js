d84f6092468ca1b2591d340f3cbe0cdd11d9ea3a
ananya-naveen <84455616+ananya-naveen@users.noreply.github.com>
2021-11-27 14:04:36 +0530

    Add files via upload

--- moustache.js
+++ moustache.js
@@ -5,12 +5,31 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.position(625,325);
+
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("Nose x = "+results[0].pose.nose.x);
+        console.log("Nose y = "+results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
 }
 
 function draw(){
-    
+    image(video,0,0,300,300);
 }
 
-function napshot(){
+function snapshot(){
     save("mustache.png");
 }
